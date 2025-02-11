22b32c08286abebc98e2f88a62661d43790f5568
ConeFriend <conefriend13@gmail.com>
2025-02-10 20:07:28 -0700

    coolio

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+const fetch = require('node-fetch'); // Make sure fetch is available
+
 module.exports = async (req, res) => {
-    const { url } = req.query;
-    
-    if (url) {
-      try {
-        const response = await fetch(url);
-        const data = await response.text();
-        
-        res.status(200).send(data); // Send the response back to the client
-      } catch (error) {
-        res.status(500).send('Error while fetching the URL');
+  const { url } = req.query;
+
+  if (url) {
+    try {
+      // Fetch the content from the provided URL
+      const response = await fetch(url);
+      
+      if (!response.ok) {
+        return res.status(500).send('Failed to fetch the URL');
       }
-    } else {
-      res.status(400).send('URL query parameter is required');
+
+      // Retrieve the content of the URL (e.g., HTML)
+      const data = await response.text();
+
+      // Send the content back to the client
+      res.status(200).send(data);
+    } catch (error) {
+      res.status(500).send('Error while fetching the URL');
     }
+  } else {
+    res.status(400).send('URL query parameter is required');
+  }
 };
-  
