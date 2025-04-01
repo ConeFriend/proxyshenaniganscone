8e516f0a8d382d738b43126b2b9ea60b0223fdf9
ConeFriend <conefriend13@gmail.com>
2025-03-31 20:56:45 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const puppeteer = require('puppeteer'); // Puppeteer will be used to render the page
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda'); // This will manage the correct Chromium version
 
 const app = express();
 
@@ -11,10 +12,13 @@ app.get('/', async (req, res) => {
     }
 
     try {
-        // Launch a new headless browser instance
+        // Launch Chromium using chrome-aws-lambda
         const browser = await puppeteer.launch({
-            headless: true, // Run in headless mode
-            args: ['--no-sandbox', '--disable-setuid-sandbox'], // Necessary for serverless environments
+            headless: true,
+            executablePath: await chromium.executablePath, // Path to the Chromium binary for serverless
+            args: chromium.args, // Arguments to make Chromium work in serverless environments
+            defaultViewport: chromium.defaultViewport, // Default viewport size
+            ignoreHTTPSErrors: true, // Ignore SSL errors
         });
 
         // Create a new page
